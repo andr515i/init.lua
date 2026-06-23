c2e15532447b2ccc2964a8220c8a905804c24912
andr515i <andr515i@zbc.dk>
2026-06-23 15:46:41 +0200

    which key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "modern",
+    delay = 500,
+    spec = {
+      { "<leader>c", group = "code" },
+      { "<leader>d", group = "debug" },
+      { "<leader>f", group = "find" },
+      { "<leader>g", group = "git" },
+      { "<leader>q", group = "quickfix" },
+      { "<leader>r", group = "run" },
+      { "<leader>x", group = "diagnostics" },
+    },
+  },
+}
